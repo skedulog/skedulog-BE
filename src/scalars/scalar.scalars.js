f28a248a1348b4d1a0e86a799135875fb60e4826
koline-1 <dnjsdn4580@gmail.com>
2023-05-19 13:59:53 +0900

    Member type&CRUD queries&mutations, datetime scalar created

---
+++ src/scalars/scalar.scalars.js
@@ -0,0 +1,7 @@
+
+import client from "../client";
+import DateTimeScalar from "./DateTimeScalar";
+
+export default {
+  DateTime : DateTimeScalar,
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const compression = require('compression');
 const schema = require('./schema').default;
-
+const { DateTimeScalar } = require('./scalars/DateTimeScalar');
 
 /* Apollo server 생성 */
 const server = new ApolloServer({ 
@@ -13,19 +13,15 @@ const server = new ApolloServer({
 /* Express app에 Apollo 연결 */
 const app = express();
 app.use(compression());
-const con = async() => {
-  await server.start();
-  server.applyMiddleware({ app, path: "/graphql" });
-}
-con();
 
 /* application 실행 확인 */
-app.listen({port: 4000}, () => {
-	console.log('Now browse to http://localhost:4000' + server.graphqlPath)
-})
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
 
-/* 기본 URL 맵핑 */
-app.get("/", (req, res) => {
-  console.log("url / has been received");
-  res.send("<h1>Hello</h1>")
-})
+  app.listen({ port: 4000 }, () => {
+    console.log(`Server ready at http://localhost:4000/graphql`);
+  });
+}
+
+startServer();
