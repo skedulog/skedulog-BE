f68bd41d839105bff3dc4d6decb197bd7356a371
koline-1 <125340956+koline-1@users.noreply.github.com>
2023-07-10 17:26:47 +0900

    set up member view & member update (#12)

--- src/member/member.queries.js
+++ src/member/member.queries.js
@@ -1,8 +1,15 @@
+import { GraphQLError } from "graphql";
 import client from "../client";
 
 export default {
   Query: {
-    member: (_, { id }) => {
+    member: (_, __, context) => {
+      const id = context.user.member.id;
+
+      if (!id) {
+        throw new GraphQLError('[member] \'id\' is null', { extensions: { http: { status: 403, code: 'MEMBER_ID_NULL',  message: '[member] \'id\' is null'} } })
+      }
+
       return client.Member.findFirst({
           where: {
             id,
